aa80ffd080f98a4258d5d02149d689f0ad4f8a5c
theBon <axel.phantom@hotmail.it>
2018-05-26 19:29:42 +0200

    Bidirectional and continuous communication
    Now ESP32 TCP client sends a string to the TCP server which answers back the same message. This passage is done every 5 seconds by closing and re-opening the connection

--- wifi_sniff_main.c
+++ wifi_sniff_main.c
@@ -130,23 +130,26 @@ void tcp_client(void *pvParam){
     tcpServerAddr.sin_family = AF_INET;
     tcpServerAddr.sin_port = htons( 3010 );
     int s, r;
+
     char recv_buf[64];
+
     while(1){
-        xEventGroupWaitBits(wifi_event_group,CONNECTED_BIT,false,true,portMAX_DELAY);
-        s = socket(AF_INET, SOCK_STREAM, 0);
-        if(s < 0) {
-            ESP_LOGE(TAG, "... Failed to allocate socket.\n");
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... allocated socket\n");
-         if(connect(s, (struct sockaddr *)&tcpServerAddr, sizeof(tcpServerAddr)) != 0) {
-            ESP_LOGE(TAG, "... socket connect failed errno=%d \n", errno);
-            close(s);
-            vTaskDelay(4000 / portTICK_PERIOD_MS);
-            continue;
-        }
-        ESP_LOGI(TAG, "... connected \n");
+    	xEventGroupWaitBits(wifi_event_group,CONNECTED_BIT,false,true,portMAX_DELAY);
+		s = socket(AF_INET, SOCK_STREAM, 0);
+		if(s < 0) {
+			ESP_LOGE(TAG, "... Failed to allocate socket.\n");
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+			continue;
+		}
+		ESP_LOGI(TAG, "... allocated socket\n");
+		 if(connect(s, (struct sockaddr *)&tcpServerAddr, sizeof(tcpServerAddr)) != 0) {
+			ESP_LOGE(TAG, "... socket connect failed errno=%d \n", errno);
+			close(s);
+			vTaskDelay(4000 / portTICK_PERIOD_MS);
+			continue;
+		}
+		ESP_LOGI(TAG, "... connected \n");
+
         if( write(s , MESSAGE , strlen(MESSAGE)) < 0)
         {
             ESP_LOGE(TAG, "... Send failed \n");
@@ -155,14 +158,19 @@ void tcp_client(void *pvParam){
             continue;
         }
         ESP_LOGI(TAG, "... socket send success");
+
         do {
             bzero(recv_buf, sizeof(recv_buf));
-            r = read(s, recv_buf, sizeof(recv_buf)-1);
+            r = read(s, recv_buf, strlen(MESSAGE));
+            printf("received from server: ");
             for(int i = 0; i < r; i++) {
                 putchar(recv_buf[i]);
             }
-        } while(r > 0);
+            printf("\n");
+        } while(r < strlen(MESSAGE));
+
         ESP_LOGI(TAG, "... done reading from socket. Last read return=%d errno=%d\r\n", r, errno);
+
         close(s);
         ESP_LOGI(TAG, "... new request in 5 seconds");
         vTaskDelay(5000 / portTICK_PERIOD_MS);
