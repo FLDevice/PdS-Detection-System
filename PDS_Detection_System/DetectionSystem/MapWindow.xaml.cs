5b452076df455809c8dd3c1e1f4d8bf19277dd69
FLDevice <flavio.lorenzo1993@gmail.com>
2019-01-29 18:31:16 +0100

    Fixed animation problem and added ESP print in map

--- PDS_Detection_System/DetectionSystem/MapWindow.xaml.cs
+++ PDS_Detection_System/DetectionSystem/MapWindow.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel; // CancelEventArgs
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using MySql.Data.MySqlClient;
-using Xceed.Wpf.Toolkit;
 
 namespace DetectionSystem
 {
@@ -25,11 +16,16 @@ namespace DetectionSystem
     {
         protected MySqlConnection DBconnection;
 
-        private int maxX, minX, maxY, minY;
+        private int maxX, minX, maxY, minY, espCount;
 
         private const int ellipseSize = 10;
 
         private long previouslyChecked = 0;
+
+        private List<ESP> eSPs;
+        
+        // Information needed for the animation
+        private long unixBegin = 0, unixLast = 0, granularity = 100;
 
         public MapWindow()
         {
@@ -43,7 +39,7 @@ namespace DetectionSystem
                 DBconnection.Open();
 
                 MySqlCommand cmm = new MySqlCommand(
-                    "SELECT MAX(x), MIN(x), MAX(y), MIN(y) FROM ESP", DBconnection);
+                    "SELECT MAX(x), MIN(x), MAX(y), MIN(y), COUNT(*) FROM ESP", DBconnection);
                 MySqlDataReader dataReader = cmm.ExecuteReader();
 
                 while (dataReader.Read())
@@ -51,11 +47,27 @@ namespace DetectionSystem
                     maxX = dataReader.GetInt32(0);
                     minX = dataReader.GetInt32(1);
                     maxY = dataReader.GetInt32(2);
-                    minY = dataReader.GetInt32(3); 
+                    minY = dataReader.GetInt32(3);
+                    espCount = dataReader.GetInt32(4);
                 }
 
                 //close Data Reader
                 dataReader.Close();
+
+                cmm = new MySqlCommand(
+                    "SELECT id, mac, x, y FROM ESP", DBconnection);
+                dataReader = cmm.ExecuteReader();
+
+                eSPs = new List<ESP>();
+                
+                while (dataReader.Read())
+                {
+                     eSPs.Add(new ESP(dataReader.GetInt32(0), dataReader.GetString(1), dataReader.GetInt32(2), dataReader.GetInt32(3)));
+                }
+
+                //close Data Reader
+                dataReader.Close();
+
                 DBconnection.Close();
             }
             catch (Exception exc)
@@ -132,18 +144,18 @@ namespace DetectionSystem
             if (beginDateTime.Text == null || lastDateTime.Text == null || granularityBox.Text == null)
                 return;
 
+            ClearMap();
+
             DateTime begin = Convert.ToDateTime(beginDateTime.Text);
             DateTimeOffset beginOffset = new DateTimeOffset(begin);
-            long unixBegin = beginOffset.ToUnixTimeSeconds();
+            unixBegin = beginOffset.ToUnixTimeSeconds();
             DateTime last = Convert.ToDateTime(lastDateTime.Text);
             DateTimeOffset lastOffset = new DateTimeOffset(last);
-            long unixLast = lastOffset.ToUnixTimeSeconds();
-            long granularity = Convert.ToInt64(granularityBox.Text);
+            unixLast = lastOffset.ToUnixTimeSeconds();
+            granularity = Convert.ToInt64(granularityBox.Text);
 
             if (unixBegin >= unixLast)
                 return;
-
-            ClearMap();
 
             try
             {
@@ -161,16 +173,17 @@ namespace DetectionSystem
                             "SELECT d.mac, d.x, d.y, d.timestamp "
                             + "FROM devices d "
                             + "JOIN( "
-                            + "SELECT mac, MAX(timestamp) timestamp "
+                            + "SELECT mac, MAX(timestamp) timestamp, MAX(dev_id) as max_id "
                             + "FROM devices "
                             + "GROUP BY mac "
-                            + ") x ON(x.mac = d.mac AND x.timestamp = d.timestamp) "
-                            + "WHERE UNIX_TIMESTAMP(d.timestamp) > " + Convert.ToString(currentTs) + " - 120 ", DBconnection);
+                            + ") x ON(x.max_id = d.dev_id) "
+                            + "WHERE UNIX_TIMESTAMP(d.timestamp) > " + Convert.ToString(currentTs) + " - 120 " 
+                            + "AND UNIX_TIMESTAMP(d.timestamp) <= " + Convert.ToString(currentTs), DBconnection);
                         MySqlDataReader dataReader = cmm.ExecuteReader();
 
                         while (dataReader.Read())
                         {
-                            DrawDevice("device" + i + "_" + Convert.ToString(currentTs), dataReader.GetString(0), dataReader.GetInt32(1), dataReader.GetInt32(2));
+                            DrawDevice("device" + i + "_" + Convert.ToString(currentTs), dataReader.GetString(0), dataReader.GetDouble(1), dataReader.GetDouble(2));
                             i++;
                         }
 
@@ -192,15 +205,41 @@ namespace DetectionSystem
                 this.Close();
             }
 
-            SetUpSlider(unixBegin, unixLast, granularity);
+            SetUpSlider();
             UpdateAnimation(unixBegin);
         }
 
         private void ClearMap()
         {
             mapOfDevices.Children.Clear();
+            for (long currentTs = unixBegin; currentTs <= unixLast; currentTs += granularity)
+            {
+                Console.WriteLine("CurrentTS " + currentTs);
+                int i = 0;
+                bool exit = false;
+                string name;
+
+                while (!exit)
+                {
+                    name = "device" + i + "_" + currentTs;
+
+                    if (mapOfDevices.FindName(name) != null)
+                        mapOfDevices.UnregisterName(name);
+                    else
+                        exit = true;
+                }
+            }
+
+            foreach (ESP curEsp in eSPs)
+            {
+                DrawESP(curEsp);
+            }
+
             timeSlider.Visibility = Visibility.Hidden;
             previouslyChecked = 0;
+            unixBegin = 0;
+            unixLast = 0;
+            granularity = 100;
         }
 
         private void DrawDevice(string deviceName, double left, double bottom)
@@ -235,8 +274,39 @@ namespace DetectionSystem
             Canvas.SetBottom(myEllipse, ScaleY(bottom));
         }
 
+        private void DrawESP(ESP curEsp)
+        {
+            // Create a red Ellipse.
+            Ellipse myEllipse = new Ellipse();
 
-        private void DrawDevice(string name, string deviceName, int left, int bottom)
+            // Create a SolidColorBrush with a red color to fill the 
+            // Ellipse with.
+            SolidColorBrush mySolidColorBrush = new SolidColorBrush();
+
+            // Describes the brush's color using RGB values. 
+            // Each value has a range of 0-255.
+            mySolidColorBrush.Color = Color.FromArgb(255, 66, 119, 244);
+            myEllipse.Fill = mySolidColorBrush;
+            myEllipse.StrokeThickness = 2;
+            myEllipse.Stroke = Brushes.Black;
+
+            // Set the width and height of the Ellipse.
+            myEllipse.Width = ellipseSize;
+            myEllipse.Height = ellipseSize;
+
+            // Add a ToolTip that shows the name of the Device and its position 
+            ToolTip tt = new ToolTip();
+            tt.Content = "ESP " + curEsp.Id + "\nMAC: " + curEsp.Mac + "\nX: " + curEsp.X + "- Y: " + curEsp.Y;
+            myEllipse.ToolTip = tt;
+
+            // Add the Ellipse to the Canvas.
+            mapOfDevices.Children.Add(myEllipse);
+
+            Canvas.SetLeft(myEllipse, ScaleX(curEsp.X));
+            Canvas.SetBottom(myEllipse, ScaleY(curEsp.Y));
+        }
+
+        private void DrawDevice(string name, string deviceName, double left, double bottom)
         {
             // Create a red Ellipse.
             Ellipse myEllipse = new Ellipse();
@@ -267,10 +337,11 @@ namespace DetectionSystem
             // Set the visibility to hidden
             myEllipse.Visibility = Visibility.Hidden;
 
-           if(mapOfDevices.FindName(name) == null)
-                mapOfDevices.RegisterName(name, myEllipse);
-            
-           // Add the Ellipse to the Canvas.
+            if (mapOfDevices.FindName(name) != null)
+                mapOfDevices.UnregisterName(name);
+            mapOfDevices.RegisterName(name, myEllipse);
+
+            // Add the Ellipse to the Canvas.
             mapOfDevices.Children.Add(myEllipse);
 
             Canvas.SetLeft(myEllipse, ScaleX(left));
@@ -309,7 +380,7 @@ namespace DetectionSystem
             UpdateAnimation(Convert.ToInt64(timeSlider.Value));
         }
 
-        private void SetUpSlider(long unixBegin, long unixLast, long granularity)
+        private void SetUpSlider()
         {
             timeSlider.Minimum = unixBegin;
             timeSlider.Maximum = unixLast;
@@ -324,7 +395,7 @@ namespace DetectionSystem
         {
             int i = 1;
             bool exit = false;
-
+            
             if (previouslyChecked != 0)
             {
                 while (!exit)
@@ -334,12 +405,10 @@ namespace DetectionSystem
                     if (device == null)
                     {
                         exit = true;
-                        Console.WriteLine("Not Found device" + i + "_" + previouslyChecked);
                     }
                     else
                     {
                         device.Visibility = Visibility.Hidden;
-                        Console.WriteLine("Hidden device" + i + "_" + previouslyChecked);
                         i++;
                     }
                 }
@@ -359,13 +428,42 @@ namespace DetectionSystem
                 else
                 {
                     device.Visibility = Visibility.Visible;
-                    Console.WriteLine("Show device" + i + "_" + previouslyChecked);
 
                     i++;
                 }
             }
 
             previouslyChecked = timestamp;
+        }
+    }
+
+    public class ESP
+    {
+        private int x, y, id;
+        public int Id
+        {
+            get { return id; }
+        }
+        public int X
+        {
+            get { return x; }
+        }
+        public int Y
+        {
+            get { return y; }
+        }
+        private string mac;
+        public string Mac
+        {
+            get { return mac; }
+        }
+
+        public ESP(int id, string mac, int x, int y)
+        {
+            this.id = id;
+            this.mac = mac;
+            this.x = x;
+            this.y = y;
         }
     }
 }
